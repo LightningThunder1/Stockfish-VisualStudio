93b238c8eae3eb01e84e5d101a0135cab27647e3
LightningThunder1 <134855228+LightningThunder1@users.noreply.github.com>
2024-08-23 10:47:01 +1000

    Update tt.h to Jan 3, 2023

--- src/tt.h
+++ src/tt.h
@@ -1,9 +1,9 @@
 /*
-  # Jan 7th, 2022, 1:45am (File version)
-  # Jan 7th, 2022, 1:45am (Current Stockfish version)
+  # Jan 3rd, 2023, 5:07am (File version)
+  # Jan 3rd, 2023, 5:07am (Current Stockfish version)
 
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2022 The Stockfish developers (see AUTHORS file)
+  Copyright (C) 2004-2023 The Stockfish developers (see AUTHORS file)
 
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
